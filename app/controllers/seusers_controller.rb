a0b1cc30903233f93884a93001f2e2fde14e3846
uu <giladbarkan@live.com>
2016-08-30 15:24:54 -0400

    debuggin paging

--- app/controllers/seusers_controller.rb
+++ app/controllers/seusers_controller.rb
@@ -1,6 +1,4 @@
 class SeusersController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:upload]
-
   def index
     respond_to do |f|
       f.json {render json: SeUser.all, status: 200}
