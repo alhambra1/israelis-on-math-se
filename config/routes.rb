324f790e470999e1cd579b1da4c4bc01ab31d437
lastromanticx <giladbarkan@live.com>
2016-08-31 01:56:38 -0400

    added root route to seusers index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'seusers#index'
+  
   get 'seusers', to: 'seusers#index'
   post 'seusers', to: 'seusers#upload'
 	get 'seusers/page', to: 'seusers#page'
